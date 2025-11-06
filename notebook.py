459debf8ad53777a862026d11d929f1a938088e6
tduplasmalab <tduplasmalab22@gmail.com>
2025-11-06 14:12:44 +0900

    パスミスってた

--- notebook.py
+++ notebook.py
@@ -37,10 +37,10 @@ def main():
     # --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
     # 1. ワークスペースとなるジオデータベースのパス
-    gdb_path = r"C:\Users\東京電機大学\Documents\ArcGIS\Projects\0925_osaka_shelter2\1030_tokyo_shelter2.gdb"
+    gdb_path = r"C:\Users\東京電機大学\Documents\ArcGIS\Projects\1030_tokyo_shelter2\1030_tokyo_shelter2.gdb"
 
     # 2. 入力フィーチャクラス名 (UTM座標系などを想定)
-    building_fc_name = "tokyo_building_point"
+    building_fc_name = "tokyo_building_point_Clip"
     shelter_fc_name = "tokyo_shelter_Merge"
 
     # 3. 出力フィーチャクラス名 (新規作成されます)
